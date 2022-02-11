bd71e752822e934a364b8647a42ab90ecb56e73c
Thiago Bignotto <bignotto@gmail.com>
2022-02-11 06:48:49 -0300

    added react hook form and refactored input component to work with it

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Control, Controller } from "react-hook-form";
 import { TextInputProps } from "react-native";
 
 import { Container } from "./styles";
 
-type Props = TextInputProps;
+interface Props extends TextInputProps {
+  control: Control;
+  name: string;
+}
 
-export function Input({ ...rest }: Props) {
-  return <Container {...rest} />;
+export function Input({ control, name, ...rest }: Props) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, onBlur, value } }) => (
+        <Container {...rest} onChangeText={onChange} value={value} />
+      )}
+    />
+  );
 }
