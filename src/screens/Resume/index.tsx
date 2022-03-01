14f9e8c1cd66b6cc188797bc8dc41fc4380b4e0a
Thiago Bignotto <bignotto@gmail.com>
2022-03-01 08:50:50 -0300

    added victory-native and implemented a pie chart in resume screen

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,39 +1,65 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useFocusEffect } from "@react-navigation/native";
 import { useCallback, useEffect, useState } from "react";
+import { RFValue } from "react-native-responsive-fontsize";
+import { useTheme } from "styled-components";
+import { VictoryPie } from "victory-native";
 import { CategorySummaryCard } from "../../components/Form/CategorySummaryCard";
 import { categories } from "../../utils/categories";
 import { CardListProps } from "../Dashboard";
-import { Container, Content, Header, Title } from "./styles";
+import { ChartContainer, Container, Content, Header, Title } from "./styles";
 
 interface CategorySum {
   [key: string]: number;
 }
 
+interface PieChartData {
+  x: string;
+  y: number;
+  color: string;
+}
+
 export function Resume() {
   const [categoriesSummary, setCategoriesSummary] = useState<CategorySum>({});
+  const [chartData, setChartData] = useState<PieChartData[]>();
+
+  const theme = useTheme();
 
   async function loadTransactions() {
     const dataKey = "@gofinances/transactions";
 
     const newSummary: CategorySum = {};
+    let outcomeTotal = 0;
 
     const storageData = await AsyncStorage.getItem(dataKey);
     const transactions: CardListProps[] = storageData
       ? JSON.parse(storageData)
       : [];
 
-    transactions.forEach((t) => {
-      if (!newSummary[t.category]) newSummary[t.category] = Number(t.amount);
-      else newSummary[t.category] += Number(t.amount);
+    const outcomeTransactions = transactions.filter(
+      (t) => t.transactionType === "outcome"
+    );
+
+    outcomeTransactions.forEach((t) => {
+      outcomeTotal += Number(t.amount);
+      if (!newSummary[t.category]) {
+        newSummary[t.category] = Number(t.amount);
+      } else newSummary[t.category] += Number(t.amount);
     });
 
+    const newChartData: PieChartData[] = Object.keys(newSummary).map((k) => {
+      const categoryColor = categories.find((c) => c.key === k);
+      return {
+        x: `${((newSummary[k] / outcomeTotal) * 100).toFixed(2)}%`,
+        y: newSummary[k],
+        color: categoryColor ? categoryColor.color : "#000000",
+      };
+    });
+
+    console.log(newChartData);
+    setChartData(newChartData);
     setCategoriesSummary(newSummary);
   }
-
-  useEffect(() => {
-    loadTransactions();
-  }, []);
 
   useFocusEffect(
     useCallback(() => {
@@ -47,6 +73,23 @@ export function Resume() {
         <Title>Resumo por Categoria</Title>
       </Header>
       <Content>
+        {chartData && (
+          <ChartContainer>
+            <VictoryPie
+              data={chartData}
+              colorScale={chartData.map((d) => d.color)}
+              style={{
+                labels: {
+                  fontSize: RFValue(14),
+                  fontWeight: "bold",
+                  fill: theme.colors.shape,
+                },
+              }}
+              labelRadius={70}
+            />
+          </ChartContainer>
+        )}
+
         {categories.map(
           (c) =>
             categoriesSummary[c.key] && (
