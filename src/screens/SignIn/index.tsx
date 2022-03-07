fa8daa47f6a977db26399b9bba78bcb05baf2550
Thiago Bignotto <bignotto@gmail.com>
2022-03-07 06:42:39 -0300

    created login screen component

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import { Container, Title, Header } from "./styles";
+
+export function SignIn() {
+  return (
+    <Container>
+      <Header>
+        <Title>Login</Title>
+      </Header>
+    </Container>
+  );
+}
