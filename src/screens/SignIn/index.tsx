1f6c08639e53cbc40dadf33af73755f1c7c5ba1e
Thiago Bignotto <bignotto@gmail.com>
2022-03-08 06:48:04 -0300

    styled login screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
 
-import { Container, Title, Header } from "./styles";
+import {
+  Container,
+  Title,
+  Header,
+  TitleWrapper,
+  SignInTitle,
+  Footer,
+} from "./styles";
+
+import AppleLogo from "../../assets/apple.svg";
+import GoogleLogo from "../../assets/google.svg";
+import AppLogo from "../../assets/logo.svg";
+
+import { RFValue } from "react-native-responsive-fontsize";
 
 export function SignIn() {
   return (
     <Container>
       <Header>
-        <Title>Login</Title>
+        <TitleWrapper>
+          <AppLogo width={RFValue(120)} height={RFValue(68)} />
+          <Title>
+            Controle{"\n"}suas finanças de{"\n"}forma simples!
+          </Title>
+        </TitleWrapper>
+        <SignInTitle>
+          Faça seu login com{"\n"}uma das contas abaixo:
+        </SignInTitle>
       </Header>
+
+      <Footer></Footer>
     </Container>
   );
 }
